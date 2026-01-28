6be3b3637c419af232e919119fb9a74d0be177de
Shivaay <Dave.mahadev66@gmail.com>
2026-01-28 13:50:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,13 +70,13 @@ st.sidebar.header("Thresholds")
 
 neg_fc = st.sidebar.select_slider(
     "Negative logFC (≤)",
-    options=[-4, -3, -2, -1],
+    options=[-10, -9, -8, -7, -6, -5, -4, -3, -2, -1],
     value=-1
 )
 
 pos_fc = st.sidebar.select_slider(
     "Positive logFC (≥)",
-    options=[1, 2, 3, 4],
+    options=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     value=1
 )
 
